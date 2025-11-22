756b323213ac71ea3237084f0c5b376588655293
Ezypoliorz <ezypoliorz.275@gmail.com>
2025-11-22 22:31:30 +0100

    New version

--- main.py
+++ main.py
@@ -38,49 +38,6 @@ async def on_ready():
         players = json.dump(players, fichier, ensure_ascii=False)
     print("Prêt !")
 
-class DropdownMenuRole(View) :
-    def __init__(self) :
-        super().__init__()
-        self.add_item(self.create_dropdown())
-    
-    def create_dropdown(self):
-        options = [
-            discord.SelectOption(label="Joueur", description="Rejoindre le serveur en tant que joueur du club"),
-            discord.SelectOption(label="Parent", description="Rejoindre le serveur en tant que parent d'un joueur du club"),
-        ]
-
-        dropdown_menu = Select(
-            placeholder = "Quel est votre rôle ?",
-            options = options,
-            custom_id = "dropdown_menu_role"
-        )
-
-        dropdown_menu.callback = self.callback_role
-        return dropdown_menu
-    
-    async def callback_role(self, interaction : discord.interactions) :
-        choice = interaction.data["values"][0]
-        await interaction.response.send_message(f"Votre rôle : **{choice}**")
-
-@bot.event
-async def on_member_join(member: discord.Member) :
-    guild = member.guild
-    name = member.display_name
-    arrival_channel = guild.system_channel
-    
-    embed = discord.Embed(
-        title = f"Bienvenue sur le serveur de Caen Alekhine, {name} !",
-        description = "Ceci est un canal d'accueil, où vous pourrez saisir vos informations. Après ça, vous aurez accès à l'entièreté du serveur !",
-        color = discord.Color.purple()
-    )
-    
-    await arrival_channel.send(embed=embed)
-
-    message = (
-        f"Merci de renseigner votre rôle au club dans la liste déroulante ci-dessous"
-    )
-    await arrival_channel.send(message, view=DropdownMenuRole())
-
 @tree.command(name="ping", description="Répond avec la latence du bot.")
 async def ping_command(interaction: discord.Interaction):
     await interaction.response.send_message(f'Pong! Latence : {round(bot.latency * 1000)}ms', ephemeral=False)
@@ -96,11 +53,10 @@ async def top_10_command(interaction: discord.Interaction):
     for index, player in enumerate(players) :
         embed.add_field(
             name=f'#{index+1} - {player["NomComplet"]}',
-            value=f'{player["Elo"]} Elo',
+            value=f'{player["Elo"][:-2]} Elo',
             inline=False
         )
     embed.set_footer(text="Bot Caen Alekhine")
-    embed.set_author(name="Bot Alekhine")
     await interaction.response.send_message(embed=embed, ephemeral=False)
 
 @tree.command(name="joueur", description="Affiche les infos d'un joueur")
@@ -119,11 +75,12 @@ async def joueur_command(interaction: discord.Interaction, nom : str):
         value=f'{player["NomComplet"]}',
         inline=False
     )
-    embed.add_field(
-        name=f'Nom d\'utilisateur Discord',
-        value=f'{player["NomDiscord"]}',
-        inline=False
-    )
+    if "NomDiscord" in player :
+        embed.add_field(
+            name=f'Nom d\'utilisateur Discord',
+            value=f'{player["NomDiscord"]}',
+            inline=False
+        )
     embed.add_field(
         name=f'Elo Standard',
         value=f'{player["Elo"]}',
@@ -140,17 +97,11 @@ async def joueur_command(interaction: discord.Interaction, nom : str):
         inline=False
     )
     embed.add_field(
-        name=f'Catégorie',
-        value=f'{player["Cat"]}',
-        inline=False
-    )
-    embed.add_field(
         name=f'N° FFE',
         value=f'{player["NrFFE"]}',
         inline=False
     )
     embed.set_footer(text="Bot Caen Alekhine")
-    embed.set_author(name="Bot Alekhine")
     await interaction.response.send_message(embed=embed, ephemeral=False)
 
 @tree.command(name="tournois", description="Affiche les prochains tournois")
@@ -168,32 +119,64 @@ async def tournois_command(interaction: discord.Interaction):
             inline=False
         )
     embed.set_footer(text="Bot Caen Alekhine")
-    embed.set_author(name="Bot Alekhine")
     await interaction.response.send_message(embed=embed, ephemeral=False)
 
-class QuattroButton(discord.ui.View) :
-    def __init__(self, timeout=180) :
-        super().__init__(timeout=timeout)
+class DropdownMenuQuattro(View) :
+    def __init__(self) :
+        super().__init__()
+        self.add_item(self.create_dropdown())
+        
     
-    @discord.ui.button(label="Test", style=discord.ButtonStyle.primary, custom_id="quattro_button")
-    async def quattro_button_callback(self, interaction : discord.Interaction, button : discord.ui.Button) :
-        user = interaction.user
-
-        await interaction.response.send_message(
-            await interaction.response.send_message(
-                f"{user.display_name}, {user.name}",
-                ephemeral=False
-            )
+    def create_dropdown(self):
+        with open("quattro.json", 'r', encoding='utf-8') as fichier :
+            données = json.load(fichier)
+        pairings = données["Appariements"]
+        options = []
+        for quattro in pairings :
+            options.append(discord.SelectOption(label=quattro))
+            
+        dropdown_menu = Select(
+            placeholder = "Sélectionnez une poule de Quattro",
+            options = options,
+            custom_id = "dropdown_menu_quattro"
         )
 
-@bot.tree.command(name="bouton_test", description="Test")
-async def bouton_test_command(interaction: discord.Interaction):
-    view_instance = QuattroButton()
+        dropdown_menu.callback = self.callback_quattro
+        return dropdown_menu
+    
+    async def callback_quattro(self, interaction : discord.Interaction) :
+        with open("quattro.json", 'r', encoding='utf-8') as fichier :
+            données = json.load(fichier)
+        pairings = données["Appariements"]
+        matches = données["Matches"]
+        dates = données["Dates"]
+        poule = interaction.data["values"][0]
 
-    await interaction.response.send_message(
-        "Test",
-        view=view_instance,
-        ephemeral=False
-    )
+        await interaction.response.defer()
 
-bot.run(os.environ.get('DISCORD_TOKEN'))
+        embed = discord.Embed(
+            title=f"Appariements {poule}",
+            color=discord.Color.orange()
+        )
+        embed.add_field(
+            name=f'Joueurs',
+            value=f'{pairings[poule][0]} • {pairings[poule][1]} • {pairings[poule][2]} • {pairings[poule][3]}',
+            inline=False
+        )
+        for i in range(6) :
+            embed.add_field(
+                name=f'Ronde {i+1} • {dates[i]}',
+                value=f'{pairings[poule][matches[i][0]]} - {pairings[poule][matches[i][1]]}\n{pairings[poule][matches[i][2]]} - {pairings[poule][matches[i][3]]}',
+                inline=False
+            )
+        embed.set_footer(text="Bot Caen Alekhine")
+
+        await interaction.delete_original_response()
+        await interaction.followup.send(embed=embed, ephemeral=False)
+
+@tree.command(name="quattro", description="Affiche les appariements du Quattro")
+async def quattro_command(interaction: discord.Interaction):
+    await interaction.response.send_message("Vous pouvez sélectionner la poule de Quattro qui vous intéresse", ephemeral=False, view=DropdownMenuQuattro())
+
+#bot.run(os.environ.get('DISCORD_TOKEN'))
+bot.run('MTQzNjA1OTE1OTI2MTg3NjQxNw.GaJkml.s3h10FIZCJbE1G9DpJ-VW8TKoyEj6vDl1WZZUs')
