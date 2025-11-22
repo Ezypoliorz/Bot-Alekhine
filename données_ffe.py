a24b80a0c7bdcbeec8eb9fe8fd19239444910f21
Ezypoliorz <ezypoliorz.275@gmail.com>
2025-11-22 18:28:18 +0100

    New version

---
+++ données_ffe.py
@@ -0,0 +1,173 @@
+import requests
+from bs4 import BeautifulSoup
+from operator import itemgetter
+import json
+import datetime
+import re
+
+URL_CLUB = 'https://www.echecs.asso.fr/ListeJoueurs.aspx?Action=JOUEURCLUBREF&ClubRef=184'
+PAYLOAD_CONSTANTES = {
+    '__EVENTTARGET': 'ctl00$ContentPlaceHolderMain$PagerFooter', 
+    '__VIEWSTATEGENERATOR': '37C7F7E6',
+}
+
+URL_FFE = "https://www.echecs.asso.fr/ListeTournois.aspx?Action=TOURNOICOMITE&ComiteRef=14"
+BASE_URL = "https://www.echecs.asso.fr/"
+ABBR_MONTH_MAP = {
+    'janv': 1, 'févr': 2, 'mars': 3, 'avr': 4,
+    'mai': 5, 'juin': 6, 'juil': 7, 'août': 8,
+    'sept': 9, 'oct': 10, 'nov': 11, 'déc': 12
+}
+
+def fetch_data(soup) :
+    lignes_joueurs = soup.find_all('tr', class_=['liste_clair', 'liste_fonce'])
+    donnees_joueurs = []
+
+    titres_colonnes = [
+        "NrFFE", "NomComplet", "Af.", "Info",
+        "Elo", "Rapide", "Blitz", "Cat", "M.", "Club"
+    ]
+
+    for ligne in lignes_joueurs :
+        cellules = ligne.find_all('td', recursive=False)
+        if len(cellules) >= len(titres_colonnes) :
+            joueur_data = {}
+            for i, titre in enumerate(titres_colonnes) :
+                texte_cellule = cellules[i].get_text(strip=True).replace('\xa0', ' ')
+                joueur_data[titre] = texte_cellule
+            joueur_data["Nom"] = "".join(joueur_data["NomComplet"].split(' ')[:-1])
+            joueur_data["Prénom"] = joueur_data["NomComplet"].split(' ')[-1]
+            donnees_joueurs.append(joueur_data)
+            
+    return donnees_joueurs
+
+
+def get_players(url) :
+    all_players_data = []
+    with requests.Session() as session :
+        response = session.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        viewstate = soup.find('input', {'__VIEWSTATE'})
+        viewstategenerator = soup.find('input', {'__VIEWSTATEGENERATOR'})
+        eventvalidation = soup.find('input', {'__EVENTVALIDATION'})
+        
+        current_payload = {
+            '__VIEWSTATE' : viewstate['value'] if viewstate else '',
+            '__VIEWSTATEGENERATOR' : viewstategenerator['value'] if viewstategenerator else PAYLOAD_CONSTANTES['__VIEWSTATEGENERATOR'],
+            '__EVENTTARGET' : PAYLOAD_CONSTANTES['__EVENTTARGET'],
+        }
+        
+        if eventvalidation :
+            current_payload['__EVENTVALIDATION'] = eventvalidation['value']
+
+        donnees_page_1 = fetch_data(soup)
+        all_players_data.extend(donnees_page_1)
+
+        page_num = 2
+        
+        while True :
+            post_data = current_payload.copy()
+            post_data['__EVENTARGUMENT'] = str(page_num) 
+
+            response = session.post(url, data=post_data)
+            response.raise_for_status()
+
+            soup = BeautifulSoup(response.text, 'html.parser')
+            nouvelles_donnees = fetch_data(soup)
+
+            if nouvelles_donnees :
+                all_players_data.extend(nouvelles_donnees)
+
+                viewstate = soup.find('input', {'__VIEWSTATE'})
+                if viewstate :
+                    current_payload['__VIEWSTATE'] = viewstate['value']
+
+                page_num += 1
+            else :
+                break
+                
+    return all_players_data
+
+def get_tournaments():
+    response = requests.get(URL_FFE)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    tournois_futurs = []
+    today = datetime.date.today()
+    current_year = None
+    
+    main_table = soup.find('table', attrs={'border': '0', 'cellspacing': '0', 'cellpadding': '4', 'width': '100%'})
+    if not main_table:
+        return []
+
+    for row in main_table.find_all('tr', recursive=False):
+        if 'RowRupture' in row.get('id', ''):
+            rupture_td = row.find('td', class_='liste_titre')
+            if rupture_td:
+                match = re.search(r'\d{4}', rupture_td.text)
+                if match:
+                    current_year = int(match.group(0))
+            continue
+            
+        if row.get('class') and (row.get('class')[0] == 'liste_fonce' or row.get('class')[0] == 'liste_clair'):
+            
+            if current_year is None:
+                continue
+
+            tds = row.find_all('td')
+            if len(tds) >= 5:
+                raw_date_str = tds[4].text.strip()
+
+                try:
+                    date_parts = raw_date_str.strip('.').split()
+                    
+                    if len(date_parts) == 2:
+                        day_str, month_abbr = date_parts
+                        month_abbr_cleaned = month_abbr.lower().replace('.', '')
+
+                        if month_abbr_cleaned in ABBR_MONTH_MAP:
+                            month = ABBR_MONTH_MAP[month_abbr_cleaned]
+                            
+                            tournament_date = datetime.date(current_year, month, int(day_str))
+
+                            if tournament_date >= today:
+                                ref = tds[0].text.strip()
+                                ville = tds[1].text.strip()
+                                departement = tds[2].text.strip()
+                                
+                                element_tournoi = tds[3].find('a')
+                                nom = element_tournoi.text.strip() if element_tournoi else "Nom non trouvé"
+                                lien = BASE_URL + element_tournoi['href'] if element_tournoi and 'href' in element_tournoi.attrs else ""
+                                
+                                tournois_futurs.append({
+                                    "Reference": ref, "Ville": ville, "Département": departement,
+                                    "NomTournoi": nom, "Date": tournament_date.strftime("%d/%m/%Y"),
+                                    "LienFiche": lien
+                                })
+
+                except ValueError:
+                    continue
+
+    return tournois_futurs
+
+def fetch_players() :
+    players = get_players(URL_CLUB)
+    players_sorted = sorted(
+        players,
+        key = itemgetter("Elo"),
+        reverse = True
+    )
+    players_indexes = {}
+    for index, player in enumerate(players_sorted) :
+        players_indexes[player["Nom"]] = index
+    with open("joueurs.json", 'w', encoding='utf-8') as file :
+        json.dump(players_sorted, file, indent=4, ensure_ascii=False)
+    with open("index_joueurs.json", 'w', encoding='utf-8') as file :
+        json.dump(players_indexes, file, indent=4, ensure_ascii=False)
+
+def fetch_tournaments() :
+    tournaments = get_tournaments()
+    with open("tournois.json", 'w', encoding='utf-8') as file :
+        json.dump(tournaments, file, indent=4, ensure_ascii=False)
---
+++ main.py
@@ -0,0 +1,198 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+from discord.ui import Select, View
+import données_ffe
+import json
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+tree = bot.tree
+
+@bot.event
+async def on_ready():
+    print(f'Connecté en tant que {bot.user} (ID: {bot.user.id})')
+    try:
+        synced = await tree.sync()
+        print(f"Synchronisé")
+    except Exception as e:
+        print(e)
+    await bot.change_presence(activity=discord.Game(name="Bot du club Caen Alekhine"))
+    données_ffe.fetch_players()
+    données_ffe.fetch_tournaments()
+    guild = bot.get_guild(1436057737657192648)
+    with open('joueurs.json', 'r', encoding='utf-8') as fichier :
+        players = json.load(fichier)
+    for member in guild.members :
+        nick = member.nick
+        if nick :
+            for player in players :
+                if player["NomComplet"].lower() == nick.lower():
+                    player["NomDiscord"] = member.name
+                    break
+    with open('joueurs.json', 'w', encoding='utf-8') as fichier :
+        players = json.dump(players, fichier, ensure_ascii=False)
+    print("Prêt !")
+
+class DropdownMenuRole(View) :
+    def __init__(self) :
+        super().__init__()
+        self.add_item(self.create_dropdown())
+    
+    def create_dropdown(self):
+        options = [
+            discord.SelectOption(label="Joueur", description="Rejoindre le serveur en tant que joueur du club"),
+            discord.SelectOption(label="Parent", description="Rejoindre le serveur en tant que parent d'un joueur du club"),
+        ]
+
+        dropdown_menu = Select(
+            placeholder = "Quel est votre rôle ?",
+            options = options,
+            custom_id = "dropdown_menu_role"
+        )
+
+        dropdown_menu.callback = self.callback_role
+        return dropdown_menu
+    
+    async def callback_role(self, interaction : discord.interactions) :
+        choice = interaction.data["values"][0]
+        await interaction.response.send_message(f"Votre rôle : **{choice}**")
+
+@bot.event
+async def on_member_join(member: discord.Member) :
+    guild = member.guild
+    name = member.display_name
+    arrival_channel = guild.system_channel
+    
+    embed = discord.Embed(
+        title = f"Bienvenue sur le serveur de Caen Alekhine, {name} !",
+        description = "Ceci est un canal d'accueil, où vous pourrez saisir vos informations. Après ça, vous aurez accès à l'entièreté du serveur !",
+        color = discord.Color.purple()
+    )
+    
+    await arrival_channel.send(embed=embed)
+
+    message = (
+        f"Merci de renseigner votre rôle au club dans la liste déroulante ci-dessous"
+    )
+    await arrival_channel.send(message, view=DropdownMenuRole())
+
+@tree.command(name="ping", description="Répond avec la latence du bot.")
+async def ping_command(interaction: discord.Interaction):
+    await interaction.response.send_message(f'Pong! Latence : {round(bot.latency * 1000)}ms', ephemeral=False)
+
+@tree.command(name="top_10", description="Affiche le top 10 du club")
+async def top_10_command(interaction: discord.Interaction):
+    with open("joueurs.json", 'r', encoding='utf-8') as fichier:
+        players = json.load(fichier)[:10]
+    embed = discord.Embed(
+        title="Classement Top 10 du Club",
+        color=discord.Color.blue()
+    )
+    for index, player in enumerate(players) :
+        embed.add_field(
+            name=f'#{index+1} - {player["NomComplet"]}',
+            value=f'{player["Elo"]} Elo',
+            inline=False
+        )
+    embed.set_footer(text="Bot Caen Alekhine")
+    embed.set_author(name="Bot Alekhine")
+    await interaction.response.send_message(embed=embed, ephemeral=False)
+
+@tree.command(name="joueur", description="Affiche les infos d'un joueur")
+async def joueur_command(interaction: discord.Interaction, nom : str):
+    with open("joueurs.json", 'r', encoding='utf-8') as fichier:
+        players = json.load(fichier)
+    with open("index_joueurs.json", 'r', encoding='utf-8') as fichier:
+        players_indexes = json.load(fichier)
+    player = players[players_indexes[nom.upper()]]
+    embed = discord.Embed(
+        title="Info joueur",
+        color=discord.Color.green()
+    )
+    embed.add_field(
+        name=f'Nom',
+        value=f'{player["NomComplet"]}',
+        inline=False
+    )
+    embed.add_field(
+        name=f'Nom d\'utilisateur Discord',
+        value=f'{player["NomDiscord"]}',
+        inline=False
+    )
+    embed.add_field(
+        name=f'Elo Standard',
+        value=f'{player["Elo"]}',
+        inline=False
+    )
+    embed.add_field(
+        name=f'Elo Rapide',
+        value=f'{player["Rapide"]}',
+        inline=False
+    )
+    embed.add_field(
+        name=f'Elo Blitz',
+        value=f'{player["Blitz"]}',
+        inline=False
+    )
+    embed.add_field(
+        name=f'Catégorie',
+        value=f'{player["Cat"]}',
+        inline=False
+    )
+    embed.add_field(
+        name=f'N° FFE',
+        value=f'{player["NrFFE"]}',
+        inline=False
+    )
+    embed.set_footer(text="Bot Caen Alekhine")
+    embed.set_author(name="Bot Alekhine")
+    await interaction.response.send_message(embed=embed, ephemeral=False)
+
+@tree.command(name="tournois", description="Affiche les prochains tournois")
+async def tournois_command(interaction: discord.Interaction):
+    with open("tournois.json", 'r', encoding='utf-8') as fichier:
+        tournaments = json.load(fichier)[:10]
+    embed = discord.Embed(
+        title="Prochains tournoi du Calvados",
+        color=discord.Color.yellow()
+    )
+    for index, tournament in enumerate(tournaments) :
+        embed.add_field(
+            name=f'{tournament["NomTournoi"]}',
+            value=f'{tournament["Date"]} - {tournament["Ville"]}',
+            inline=False
+        )
+    embed.set_footer(text="Bot Caen Alekhine")
+    embed.set_author(name="Bot Alekhine")
+    await interaction.response.send_message(embed=embed, ephemeral=False)
+
+class QuattroButton(discord.ui.View) :
+    def __init__(self, timeout=180) :
+        super().__init__(timeout=timeout)
+    
+    @discord.ui.button(label="Test", style=discord.ButtonStyle.primary, custom_id="quattro_button")
+    async def quattro_button_callback(self, interaction : discord.Interaction, button : discord.ui.Button) :
+        user = interaction.user
+
+        await interaction.response.send_message(
+            await interaction.response.send_message(
+                f"{user.display_name}, {user.name}",
+                ephemeral=False
+            )
+        )
+
+@bot.tree.command(name="bouton_test", description="Test")
+async def bouton_test_command(interaction: discord.Interaction):
+    view_instance = QuattroButton()
+
+    await interaction.response.send_message(
+        "Test",
+        view=view_instance,
+        ephemeral=False
+    )
+
+bot.run(DISCORD_TOKEN)
